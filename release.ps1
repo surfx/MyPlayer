aea9a4640fd6b4c27bfe50e9acb280f2f67f49a2
Emerson Shigueo Sugimoto <emerson.sugimoto@serpro.gov.br>
2026-06-10 19:46:18 -0300

    release.ps1

--- release.ps1
+++ release.ps1
@@ -1,13 +1,17 @@
 # Configurações
 $projectName = "MyPlayer"
 $projectPath = ".\MyPlayer\MyPlayer.csproj"
-$outputDir = ".\MyPlayer\bin\Release\net10.0-windows"
+$outputDir = ".\MyPlayer\bin\Release\net10.0"
 
 Write-Host "Iniciando processo de release para $projectName..." -ForegroundColor Cyan
 
+# Finalizando processos ativos para evitar lock de arquivo
+Write-Host "Verificando se $projectName está em execução..." -ForegroundColor Gray
+Get-Process $projectName -ErrorAction SilentlyContinue | Stop-Process -Force
+
 # Executando dotnet publish
 Write-Host "Executando dotnet publish..." -ForegroundColor Green
-dotnet restore --source https://api.nuget.org/v3/index.json --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet10/nuget/v3/index.json
+dotnet restore $projectPath --source https://api.nuget.org/v3/index.json --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet10/nuget/v3/index.json
 dotnet publish $projectPath -c Release -o $outputDir --self-contained false
 
 if ($LASTEXITCODE -eq 0) {
